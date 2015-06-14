88ac8480f6b5ee0ea31ee0b83e1ffcc2a4945978
swong838 <sw.advocate@gmail.com>
2015-06-14 15:19:23 -0700

    Basically works.
    temp->color, light->brightness works OK, sensors calibrated for bench
    mode. Outputs readings on serial, 100ms update interval.

--- nightlight.ino
+++ nightlight.ino
@@ -3,53 +3,68 @@
 /* LED info */
 const int       
     NUMPIXELS = 1,
-    PIXEL_PIN = 2,
-    THERMAL_PIN = 3,
+    PIXEL_PIN = 3,
+    THERMAL_PIN = 1,
     LUX_PIN = 4,
-    INTERVAL = 5000;
+    INTERVAL = 100,
+    SENSOR_SKEW = 14,
+    MAX_C = 35,
+    MIN_C = 15;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 int getTemperature(){
-    int temp_C = 20;
-    int raw = analogRead(THERMAL_PIN);
     // from https://learn.adafruit.com/tmp36-temperature-sensor/using-a-temp-sensor
-    int voltage = (raw) * (5000/1024);
-    temp_C = (voltage - 500) / 10;
+    int temp_C = 22,
+        raw = analogRead(THERMAL_PIN),
+        voltage = (raw) * (5000/1024);
+    temp_C = ((voltage - 500) / 10) + SENSOR_SKEW;
     return temp_C;
 }
-uint32_t getColorFromTemperature(temp_C){
-/*
-this.clip - need function to clip range from 0-255
-    t = (temp - 22) * 10;
-    r = this.clip((128 - t) * 2);
-    g = this.clip((128 - Math.abs(t)) * 2);
-    b = this.clip((128 + t) * 2);
-*/
-
-   return pixels.Color(255,255,255); 
+uint32_t getColorFromTemperature(){
+    float temp_C = getTemperature(),
+          t = (temp_C - 25) * 10;
+    int 
+        r = constrain((128 - t) * 2, 0, 255),
+        g = constrain((128 - abs(t)) * 2, 0, 255),
+        b = constrain((128 + t) * 2, 0, 255);
+   return pixels.Color(r, g, b); 
 }
 
-void applyLighting(uint32_t targetColor) {
+int getBrightness(){
+    int input = analogRead(LUX_PIN) - 128;
+    input = constrain(input, 64, 255);
+    return input;
+}
+
+void applyLighting(uint32_t targetColor, int brightness) {
     int i;
     for(i=0; i < NUMPIXELS; i++) {
         pixels.setPixelColor(i, targetColor);
     }
+    pixels.setBrightness(brightness);
     pixels.show();
 }
 
 void setup(){
     randomSeed(analogRead(0));
+    pinMode(PIXEL_PIN, OUTPUT);
     pinMode(THERMAL_PIN, INPUT);
+    pinMode(LUX_PIN, INPUT);
 #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
     // init neopixel library
-    pixels.begin(); 
+    pixels.begin();
+    Serial.begin(9600);
 }
 void loop(){
-    int temp_C = getTemperature();
-    uint32_t newColor = getColorFromTemperature(temp_C);
-    applyLighting(newColor);
+    uint32_t color = getColorFromTemperature();
+    int brightness = getBrightness();
+    applyLighting(color, brightness);
+    Serial.print("Temperature = ");
+    Serial.println(getTemperature());
+    Serial.print("Light = ");
+    Serial.println(getBrightness());
     delay(INTERVAL);
 }
