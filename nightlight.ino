ba42c021ca75ae2bad7a64cffc18f9753b1e83c9
swong838 <sw.advocate@gmail.com>
2015-07-27 19:19:13 -0700

    updated pin mappings for Trinket
    generally works OK, needs some easing on sensor changes

--- nightlight.ino
+++ nightlight.ino
@@ -3,36 +3,38 @@
 /* LED info */
 const int       
     NUMPIXELS = 1,
-    PIXEL_PIN = 3,
-    THERMAL_PIN = 1,
+    PIXEL_PIN = 1,
+    THERMAL_PIN = 2,
+    THERMAL_DATA_PIN = 1,
     LUX_PIN = 4,
-    INTERVAL = 100,
+    LUX_DATA_PIN = 2,
+    INTERVAL = 250,
     SENSOR_SKEW = 14,
-    MAX_C = 35,
-    MIN_C = 15;
+    OPTIMAL_C = 28;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 int getTemperature(){
     // from https://learn.adafruit.com/tmp36-temperature-sensor/using-a-temp-sensor
     int temp_C = 22,
-        raw = analogRead(THERMAL_PIN),
+        raw = analogRead(THERMAL_DATA_PIN),
         voltage = (raw) * (5000/1024);
     temp_C = ((voltage - 500) / 10) + SENSOR_SKEW;
     return temp_C;
 }
 uint32_t getColorFromTemperature(){
     float temp_C = getTemperature(),
-          t = (temp_C - 25) * 10;
+          t = (temp_C - OPTIMAL_C) * 10;
     int 
         r = constrain((128 - t) * 2, 0, 255),
         g = constrain((128 - abs(t)) * 2, 0, 255),
         b = constrain((128 + t) * 2, 0, 255);
+
    return pixels.Color(r, g, b); 
 }
 
 int getBrightness(){
-    int input = analogRead(LUX_PIN) - 128;
+    int input = analogRead(LUX_DATA_PIN) - 128;
     input = constrain(input, 64, 255);
     return input;
 }
@@ -47,24 +49,18 @@ void applyLighting(uint32_t targetColor, int brightness) {
 }
 
 void setup(){
-    randomSeed(analogRead(0));
     pinMode(PIXEL_PIN, OUTPUT);
     pinMode(THERMAL_PIN, INPUT);
-    pinMode(LUX_PIN, INPUT);
+    //pinMode(LUX_PIN, INPUT);
 #if defined (__AVR_ATtiny85__)
     if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
     // init neopixel library
     pixels.begin();
-    Serial.begin(9600);
+
 }
 void loop(){
     uint32_t color = getColorFromTemperature();
-    int brightness = getBrightness();
-    applyLighting(color, brightness);
-    Serial.print("Temperature = ");
-    Serial.println(getTemperature());
-    Serial.print("Light = ");
-    Serial.println(getBrightness());
+    applyLighting(color, getBrightness());
     delay(INTERVAL);
 }
